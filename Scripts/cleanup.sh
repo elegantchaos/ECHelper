2c7161110aa076407e897d6caa01fddd74a32788
Sam Deane <sam@elegantchaos.com>
2011-12-15 21:10:18 +0000

    added unload to cleanup

--- Scripts/cleanup.sh
+++ Scripts/cleanup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# tell the system to unload the helper
+sudo launchctl unload /Library/LaunchDaemons/com.elegantchaos.helper.helper.plist
+
 # remove helper
 sudo rm -f /Library/PrivilegedHelperTools/com.elegantchaos.helper.helper 
 
