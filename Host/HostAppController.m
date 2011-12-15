02f0843eb83e5a7ab4186f8e6b408f70961fd1bd
Sam Deane <sam@elegantchaos.com>
2011-12-15 18:20:36 +0000

    more cleanup

--- Host/HostAppController.m
+++ Host/HostAppController.m
@@ -1,67 +1,25 @@
-/*
- 
-    File: SMJobBlessAppController.m
-Abstract: The main application controller. When the application has finished
-launching, the helper tool will be installed.
- Version: 1.2
-
-Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
-Inc. ("Apple") in consideration of your agreement to the following
-terms, and your use, installation, modification or redistribution of
-this Apple software constitutes acceptance of these terms.  If you do
-not agree with these terms, please do not use, install, modify or
-redistribute this Apple software.
-
-In consideration of your agreement to abide by the following terms, and
-subject to these terms, Apple grants you a personal, non-exclusive
-license, under Apple's copyrights in this original Apple software (the
-"Apple Software"), to use, reproduce, modify and redistribute the Apple
-Software, with or without modifications, in source and/or binary forms;
-provided that if you redistribute the Apple Software in its entirety and
-without modifications, you must retain this notice and the following
-text and disclaimers in all such redistributions of the Apple Software.
-Neither the name, trademarks, service marks or logos of Apple Inc. may
-be used to endorse or promote products derived from the Apple Software
-without specific prior written permission from Apple.  Except as
-expressly stated in this notice, no other rights or licenses, express or
-implied, are granted by Apple herein, including but not limited to any
-patent rights that may be infringed by your derivative works or by other
-works in which the Apple Software may be incorporated.
-
-The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
-MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
-THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
-FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
-OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
-
-IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
-OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
-MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
-AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
-STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
-POSSIBILITY OF SUCH DAMAGE.
-
-Copyright (C) 2011 Apple Inc. All Rights Reserved.
-
- 
-*/
 
 #import <ServiceManagement/ServiceManagement.h>
 #import <Security/Authorization.h>
-#import "ECBlessAppController.h"
+#import "HostAppController.h"
 
-@implementation SMJobBlessAppController
+@implementation HostAppController
+
 - (void)applicationDidFinishLaunching:(NSNotification *)notification
 {
+    // look up the helper bundle id in our plist
+    // (we're assuming that it's the one and only key inside the SMPrivilegedExecutables dictionary)
     NSDictionary* helpers = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"SMPrivilegedExecutables"];
     NSString* helperID = [[helpers allKeys] objectAtIndex:0];
     
-	NSError *error = nil;
-	if (![self blessHelperWithLabel:helperID error:&error]) {
+	NSError* error = nil;
+	if (![self blessHelperWithLabel:helperID error:&error]) 
+    {
 		NSLog(@"Something went wrong!");
-	} else {
+        [[NSApplication sharedApplication] presentError:error];
+	} 
+    else
+    {
 		/* At this point, the job is available. However, this is a very
 		 * simple sample, and there is no IPC infrastructure set up to
 		 * make it launch-on-demand. You would normally achieve this by
