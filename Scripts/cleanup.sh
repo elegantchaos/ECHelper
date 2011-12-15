581087603da2af10d38f275ef50cc210faee30bf
Sam Deane <sam@elegantchaos.com>
2011-12-15 19:55:53 +0000

    fixed cleanup script

--- Scripts/cleanup.sh
+++ Scripts/cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # remove helper
-sudo rm /Library/PrivilegedHelperTools/com.elegantchaos.helper.helper 
+sudo rm -f /Library/PrivilegedHelperTools/com.elegantchaos.helper.helper 
 
 # remove launchctl plist
-sudo rm /Library/LaunchDaemon/com.elegantchaos.helper.helper
+sudo rm -f /Library/LaunchDaemons/com.elegantchaos.helper.helper.plist
