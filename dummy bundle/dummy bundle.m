4ee943cf22445fcdf292a4e759c60772388038fd
Sam Deane <sam@elegantchaos.com>
2011-12-15 13:32:59 +0000

    added hack to set signing id automatically

---
+++ dummy bundle/dummy bundle.m
@@ -0,0 +1,13 @@
+//
+// Prefix header for all source files of the 'dummy bundle' target in the 'dummy bundle' project
+//
+
+#ifdef __OBJC__
+    #import <Cocoa/Cocoa.h>
+#endif
+
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
